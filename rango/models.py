86c952a4b86e01712a1a8672f62a25372c3ea174
kmcodes <mumbaimenon@gmail.com>
2014-08-17 01:11:00 +0530

    Many many changes

--- rango/models.py
+++ rango/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    view = models.IntegerField(default=0, unique=False)
+    likes = models.IntegerField(default=0, unique=False)
 
     def __unicode__(self):
         return self.name
@@ -11,8 +12,8 @@ class Page(models.Model):
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=128)
     url = models.URLField()
-    views = models.IntegerField(default=0)
-    
+    views = models.IntegerField(default=0, unique=False)
+
     def __unicode__(self):
         return self.title
 
